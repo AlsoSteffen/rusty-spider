877a27ecfed7dc6a483a53af70d7470bb96341b6
steff <steffen.tourbier@student.nhlstenden.com>
2024-08-27 22:05:34 +0200

    Add initial files

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use]
+extern crate rocket;
+
+mod routes;
+mod services;
+
+use std::collections::HashMap;
+use rocket::fs::{relative, FileServer, Options};
+use crate::routes::date::{date_plus_month, get_current_date};
+use rocket_dyn_templates::{context, Template};
+
+
+#[get("/")]
+fn index() -> Template {
+    let context = context! {
+        title_main: "🍺 Tourbier",
+    };
+
+    Template::render("pages/index", &context)}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/static", FileServer::from(relative!("static")))
+        .attach(Template::fairing())
+}
